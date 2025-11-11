2e19f3272a900434dba610f2e1a63fa4e2ae6513
Gabriele <gmercolino2003@gmail.com>
2025-11-11 12:32:45 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "SwingInput dev flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [jdk21];
+          shellHook = ''
+            clear
+            echo "SwingInput dev env activated" | ${pkgs.lolcat}/bin/lolcat
+          '';
+        };
+      }
+    );
+}
